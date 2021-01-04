61b0ef844ae62d3d7ee16ff575e81f31a4a87ace
Deepak Kumar <prajapati1999.in@gmail.com>
2021-01-04 16:16:46 +0530

    Splash Activity, SignIn Activity Added

---
+++ app/src/main/java/com/riseinsteps/packbagbuddy/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.riseinsteps.packbagbuddy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    private TextView createAccount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        createAccount = findViewById(R.id.tv_CreateAccount);
+
+        createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignInActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/riseinsteps/packbagbuddy/SplashActivity.java
@@ -0,0 +1,44 @@
+package com.riseinsteps.packbagbuddy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private ImageView appLogo;
+    private Animation animation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // for hiding the status bar at the top
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        appLogo = findViewById(R.id.appLogo);
+        animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.animation_bottom);
+
+        appLogo.startAnimation(animation);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }).start();
+
+    }
+}
