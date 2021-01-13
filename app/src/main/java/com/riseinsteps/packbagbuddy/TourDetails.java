729ebb7a8cb5ad28382314043e9eaf52e11f5571
Deepak Kumar <prajapati1999.in@gmail.com>
2021-01-13 12:12:56 +0530

    TourDetailsActivity Added

--- app/src/main/java/com/riseinsteps/packbagbuddy/TourDetails.java
+++ app/src/main/java/com/riseinsteps/packbagbuddy/TourDetails.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Tour_Details extends AppCompatActivity {
+public class TourDetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
