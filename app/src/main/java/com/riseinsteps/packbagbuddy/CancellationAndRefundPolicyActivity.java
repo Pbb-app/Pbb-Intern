bdd3942ef61dd3b0dd74278aea519883d659d952
Deepak Kumar <prajapati1999.in@gmail.com>
2021-01-14 22:10:21 +0530

    Policy Added

---
+++ app/src/main/java/com/riseinsteps/packbagbuddy/CancellationAndRefundPolicyActivity.java
@@ -0,0 +1,14 @@
+package com.riseinsteps.packbagbuddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CancellationAndRefundPolicyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cancellation_and_refund_policy);
+    }
+}
---
+++ app/src/main/java/com/riseinsteps/packbagbuddy/CopyRightPolicyActivity.java
@@ -0,0 +1,14 @@
+package com.riseinsteps.packbagbuddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CopyRightPolicyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_copy_right_policy);
+    }
+}
