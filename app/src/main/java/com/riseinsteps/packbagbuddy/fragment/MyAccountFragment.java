09979d9201549bbf5e75497213bbcb6b0970191d
SAKSHAM-SUDO <54800729+SAKSHAM-SUDO@users.noreply.github.com>
2021-02-04 22:00:17 +0530

    image upload on firebase

--- app/src/main/java/com/riseinsteps/packbagbuddy/fragment/MyAccountFragment.java
+++ app/src/main/java/com/riseinsteps/packbagbuddy/fragment/MyAccountFragment.java
@@ -1,8 +1,10 @@
 package com.riseinsteps.packbagbuddy.fragment;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,6 +22,8 @@ import androidx.fragment.app.Fragment;
 
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -28,6 +32,9 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.riseinsteps.packbagbuddy.LoginActivity;
 import com.riseinsteps.packbagbuddy.R;
 import com.riseinsteps.packbagbuddy.model.User;
@@ -37,19 +44,22 @@ public class MyAccountFragment extends Fragment {
 
     FirebaseUser mCurrentUser;
     private DatabaseReference databaseReference;
+    StorageReference storageReference;
+    FirebaseDatabase database;
     String name, emailId, phoneNumber, username;
     Uri photoUrl;
     ImageView profileImage;
     TextView tvFullName, tvUserEmailId, tvUserPhone, tvUserName;
     private FirebaseAuth mAuth;
     View view;
-    Button signout;
+    Button signout, btnImageChange;
 
     @Nullable
     @Override
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_my_account, container, false);
+        storageReference= FirebaseStorage.getInstance().getReference();
         initComponenet();
         mCurrentUser = mAuth.getCurrentUser();
         if (mCurrentUser != null) {
@@ -69,7 +79,47 @@ public class MyAccountFragment extends Fragment {
                 startActivity(intent);
             }
         });
+        btnImageChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent openGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(openGallery,100);
+            }
+        });
         return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==100)
+        {
+            if(resultCode == Activity.RESULT_OK)
+            {
+                Uri imageUri= data.getData();
+                profileImage.setImageURI(imageUri);
+                storeImageToFireBase(imageUri);
+            }
+        }
+    }
+
+    private void storeImageToFireBase(Uri imageUri)
+    {
+        StorageReference filerefrence = storageReference.child("profile_image");
+        filerefrence.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(getContext(),"Image Uploaded",Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getContext(),"Error",Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 
     private void fetchDetatils() {
@@ -108,6 +158,6 @@ public class MyAccountFragment extends Fragment {
         profileImage = view.findViewById(R.id.profileimage);
         signout= view.findViewById(R.id.btnsignout);
         tvUserName = view.findViewById(R.id.tvUserName);
-
+        btnImageChange= view.findViewById(R.id.btnChangeImage);
     }
 }
